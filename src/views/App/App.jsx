df8ffd438615b9f0ee3a9c443b5eede83536dbb8
William Bolton <william.ellet@gmail.com>
2020-09-12 13:01:24 -0400

    Add React Router and welcome page view

--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
 
-// Bootstrap components
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-
 // Custom components
+import Routes from 'config/Routes';
 import Footer from 'components/Footer';
 import NavigationBar from 'components/NavigationBar';
 
 const App = () => (
   <div>
     <NavigationBar />
-    <Container className="p-3">
-      <Jumbotron>
-        <h1 className="header">
-          The Digital SEL
-        </h1>
-        <Button>Hi!!!</Button>
-      </Jumbotron>
-    </Container>
+    <Routes />
     <Footer />
   </div>
 );
---
+++ src/views/Welcome/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+
+const Welcome = () => (
+  <Container className="p-3">
+    Test
+  </Container>
+);
+
+export default Welcome;
